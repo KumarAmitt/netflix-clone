f6bce5333f01c84e5ae34288a5be68e1233e517f
KumarAmitt <amitt2015@outlook.com>
2021-11-17 20:09:49 +0530

    Add Trailer link

--- src/Row.js
+++ src/Row.js
@@ -1,11 +1,15 @@
 import React, {useEffect, useState} from 'react';
 import axios from './axios';
+import YouTube from 'react-youtube';
+import movieTrailer from 'movie-trailer';
+
 import './Row.css';
 
 const base_url = 'https://image.tmdb.org/t/p/original';
 
 const Row = ({title, fetchURL, isLargeRow}) => {
   const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState('');
 
   useEffect(() => {
     const fetchData = async() => {
@@ -16,7 +20,29 @@ const Row = ({title, fetchURL, isLargeRow}) => {
 
   }, [fetchURL])
 
+  const opts = {
+    height: '390',
+    width: '100%',
+    playerVars: {
+      autoplay: 1
+    }
+  }
+
   console.log(movies)
+
+  const handleClick = (movie) => {
+    if(trailerUrl) {
+      setTrailerUrl('')
+    }else {
+      movieTrailer(movie?.name || '')
+          .then(url => {
+            const urlParams = new URLSearchParams(new URL(url).search);
+            setTrailerUrl(urlParams.get('v'));
+          })
+          .catch(error => console.log(error))
+    }
+  }
+
 
   return (
       <div className="row">
@@ -24,10 +50,19 @@ const Row = ({title, fetchURL, isLargeRow}) => {
         <div className="row__posters">
           {
             movies.map(movie => (
-                <img key={movie.id} src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} alt={movie.title} className={`row__poster ${isLargeRow && "row__posterLarge"}`} />
+                <img
+                    key={movie.id}
+                    src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
+                    alt={movie.title}
+                    className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+                    onClick={() => handleClick(movie)}
+                />
             ))
           }
         </div>
+        {
+          trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />
+        }
       </div>
   );
 };
