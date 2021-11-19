9ad2346badcc84a06a1fac799f1e412643c22a8f
KumarAmitt <amitt2015@outlook.com>
2021-11-19 22:58:19 +0530

    Add api directory

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Row from './components/Row';
-import requests from "./requests";
+import requests from "./api/requests";
 import Banner from "./components/Banner";
 import Nav from './components/Nav'
 
